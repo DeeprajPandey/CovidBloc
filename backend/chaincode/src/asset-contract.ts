d31cd5b718387fd5e758c5f35a8bf8fcd4514a76
agrawalnandini <nandini.agrawal03@gmail.com>
2020-07-01 02:01:34 +0530

    Add functions to main asset-contract.ts

--- backend/chaincode/src/asset-contract.ts
+++ backend/chaincode/src/asset-contract.ts
@@ -5,6 +5,7 @@
 import { Context, Contract, Info, Returns, Transaction } from 'fabric-contract-api';
 import { Meta, DailyKey } from './asset';
 import { HealthOfficer, Patient, Approval } from './asset';
+const ClientIdentity = require('fabric-shim').ClientIdentity; 
 
 @Info({title: 'AssetContract', description: 'My Smart Contract' })
 export class AssetContract extends Contract {
@@ -25,16 +26,18 @@ export class AssetContract extends Contract {
         const updatedMeta = new Meta();
         updatedMeta.patientCtr = lastPatientID + 1;
         await this.updateAsset(ctx, 'meta', JSON.stringify(updatedMeta));
+
     }
 
     @Transaction()
     public async initiateState(ctx: Context): Promise<void> {
-        const meta_buffer = Buffer.from(JSON.stringify(new Meta()));
-        await ctx.stub.putState("meta", meta_buffer);
+        //const meta_buffer = Buffer.from(JSON.stringify(new Meta()));
+        //await ctx.stub.putState("meta", meta_buffer);
 
         const medProf = new HealthOfficer();
         medProf.name = "New Name";
         medProf.hospital = "Apollo";
+        //medProf.email="m1@apollo.com";
         const medBuff = Buffer.from(JSON.stringify(medProf));
         await ctx.stub.putState("m123", medBuff);
 
@@ -46,7 +49,11 @@ export class AssetContract extends Contract {
                 {hexkey: "33917c36d48744ef3fbc4985188ea9e2", i: 2655360}
             ]
         };
-        await this.addPatient(ctx, patientFromServer);
+
+        //await this.validatePatient(ctx,"123","1231312")
+        //await this.addPatient(ctx, patientFromServer);
+        await this.getMedProfile(ctx,"123");
+        //await this.getKeys(ctx);
     }
 
     @Transaction(false)
@@ -124,6 +131,73 @@ export class AssetContract extends Contract {
             throw new Error(`The asset ${assetId} does not exist`);
         }
         await ctx.stub.deleteState(assetId);
+    }
+
+    /**
+     * Query a HealthOfficer Asset from the WS.
+     * 
+     * @param ctx Transactional context
+     * @param medID Unique ID of the health officer 
+     */
+    @Transaction()
+    public async getMedProfile(ctx: Context, medID:string){
+        const cid = new ClientIdentity(ctx.stub);
+        const userID = cid.getID(); 
+        const medKey= "m" + medID;
+        const medObj = await this.readAsset(ctx,medKey);
+        if(medObj!=null){ // && medObj.email==userID){
+            return medObj;
+        }
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * Validating before adding +ve patient details to the WS.
+     * 
+     * @param ctx Transactional context
+     * @param medID Unique ID of the health officer 
+     * @param checkID ApprovalID of the patient 
+     */
+    @Transaction()
+    public async validatePatient(ctx: Context, medID:string, checkID:string){
+        const medKey = "m" + medID;
+        const medObj= await this.readAsset(ctx,medKey);
+        if(medObj!=null){
+            for (let i = 1; i <= medObj.approvalCtr; i++) {
+                const assetKey= medKey+":"+i.toString();
+                const approvalObj = await this.readAsset(ctx,assetKey);
+                if(approvalObj!=null && approvalObj.approvalID==checkID && approvalObj.patientID==null){
+                    return "Validate Patient Successful";
+                }
+            }
+            throw new Error(`ApprovalID ${checkID} is invalid`);
+        }
+
+        else{
+            throw new Error(`MedicalID ${medID} is invalid`);
+        }
+    }
+
+    /**
+     * Downloading all the +ve patient keys from the WS.
+     * 
+     * @param ctx Transactional context
+     */
+    @Transaction()
+    public async getKeys(ctx:Context){
+        const allResults= [];
+        const key = "meta";
+        const metaObj = await this.readAsset(ctx,key);
+        for (let i=1;i<=metaObj.num_positives;i++){
+            const patientKey = "p"+i.toString();
+            const patientObj = await this.readAsset(ctx,patientKey);
+            if(patientObj!=null){
+                allResults.push(patientObj.dailyKeys);  //if key also needs to sent then {patientKey,patientObj}
+            }
+        }
+        return JSON.stringify(allResults);
     }
 
 }
