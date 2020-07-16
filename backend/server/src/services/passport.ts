1b42ea04673c937c33e69d71a427d5d0bc319914
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-07-16 20:40:31 +0530

    Connect to WS after checks pass and get profile to return to server

--- backend/server/src/services/passport.ts
+++ backend/server/src/services/passport.ts
@@ -1,7 +1,10 @@
+// @ts-nocheck
 import * as dotenv from 'dotenv';
 import fs from 'fs';
 import * as path from 'path';
 import { Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt';
+import * as fabric from './fabric';
+import { NetworkObject, GenericResponse } from './fabric.interface';
 import HealthOfficialModel from '../models/HealthOfficial';
 
 dotenv.config();
@@ -40,22 +43,38 @@ const verifyCallback = async function(jwt_payload: any, done: any) {
     }
 
     // If the user has not registered, we won't find their asset in WS
-    if (dbObj.t_status === "UNREGISTERED") {
+    if (dbObj.t_status === "UNREGISTERED" || (dbObj.medID === "-1")) {
       done(null, false, {
         r: "unregistered"
       });
     }
 
     // User has registered
-    // TODO: Connect to Fabric and get info
+    const medProfile = await getMedProfile(official_email, dbObj.medID);
+    done(null, medProfile); // login successful
   } catch (err) {
     done(err, false);
   }
 }
 
+async function getMedProfile(email: string, med_id: string) {
+  const networkObj: GenericResponse | NetworkObject = await fabric.connectAsUser(email);
+  if (networkObj.err !== null || !("gateway" in networkObj)) {
+    console.error(`passport.getMedInfo::${networkObj.err}`);
+    throw new Error("Invalid request");
+  }
+  const contractResponse = await fabric.invoke('getMedProfile', [med_id], true, networkObj);
+  networkObj.gateway.disconnect();
+  if ("err" in contractResponse) {
+    console.error(contractResponse.err);
+    // Transaction error
+    throw new Error("Something went wrong, please try again");
+  }
+
+  // data prop has the HealthOfficial asset
+  return contractResponse.data;
+}
+
 export = async (passport: any) => {
   passport.use(new JwtStrategy(options, verifyCallback));
 };
-// export function m() {
-//   console.log(PUBKEY);
-// }
