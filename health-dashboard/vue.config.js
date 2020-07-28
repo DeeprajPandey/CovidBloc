06ab4c75e3c7a56f7dc001f873f87e3405f65025
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-07-28 16:28:22 +0530

    Fix merge conflict and change chaincode version

--- health-dashboard/vue.config.js
+++ health-dashboard/vue.config.js
@@ -11,11 +11,7 @@ module.exports = {
     ]
   },
   pwa: {
-<<<<<<< HEAD
     name: 'Exposure Notification on Fabric',
-=======
-    name: 'Vue Argon Dashboard',
->>>>>>> dev-app
     themeColor: '#172b4d',
     msTileColor: '#172b4d',
     appleMobileWebAppCapable: 'yes',
