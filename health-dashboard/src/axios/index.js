7fb4e108663e37355a755f43ae39ae56d65f367b
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-07-30 05:49:43 +0530

    Add nloader and use it before and after axios calls

--- health-dashboard/src/axios/index.js
+++ health-dashboard/src/axios/index.js
@@ -3,5 +3,21 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseUrl: 'http://localhost:6400/'
 });
+// eslint-disable-next-line
+NProgress.configure({ minimum: 0.4, easing: 'ease', speed: 1500, showSpinner: false });
+
+// before a request is made start the nprogress
+axiosInstance.interceptors.request.use(config => {
+  // eslint-disable-next-line
+  NProgress.start()
+  return config
+})
+
+// before a response is returned stop nprogress
+axiosInstance.interceptors.response.use(response => {
+  // eslint-disable-next-line
+  NProgress.done()
+  return response
+})
 
 export { axiosInstance };
