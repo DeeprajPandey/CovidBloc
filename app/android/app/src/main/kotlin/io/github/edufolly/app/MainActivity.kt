ada2b9adbfcef073823d2882f46f1f6c21d5b10b
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2020-08-02 15:25:50 +0530

    Remove app and backend code

--- app/android/app/src/main/kotlin/io/github/edufolly/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.edufolly.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- backend/chaincode/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { AssetContract } from './asset-contract';
-export { AssetContract } from './asset-contract';
-
-export const contracts: any[] = [ AssetContract ];
